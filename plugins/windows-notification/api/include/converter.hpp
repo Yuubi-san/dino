a854d50e98a8a04a3ce43f3a089b7c82aa07c0e8
mjk <yuubi-san@users.noreply.github.com>
2021-03-02 19:19:06 +0300

    add utf8<->utf16 converters for more types

--- plugins/windows-notification/api/include/converter.hpp
+++ plugins/windows-notification/api/include/converter.hpp
@@ -2,7 +2,44 @@
 
 #include <string>
 #include <string_view>
+#include <memory>
+#include <type_traits>
 #include <glib.h>
 
-std::wstring sview_to_wstr(const std::string_view str);
-gchar* wsview_to_char(const std::wstring_view wstr);
+namespace glib
+{
+    template<typename T>
+    struct deleter
+    {
+        static_assert(std::is_trivially_destructible_v<std::remove_extent_t<T>>);
+        void operator()(gpointer const p) const
+        {
+            g_free(p);
+        }
+    };
+
+    struct string_deleter
+    {
+        void operator()(GString* const s) const
+        {
+            if (s)
+                g_string_free(s,TRUE);
+        }
+    };
+
+    template<typename T>
+    using unique_ptr = std::unique_ptr<T, deleter<T>>;
+    using string_ptr = std::unique_ptr<GString, string_deleter>;
+}
+
+std::string wsview_to_str(std::wstring_view wstr);
+glib::string_ptr wsview_to_gstr(std::wstring_view wstr);
+glib::unique_ptr<gchar[]> wsview_to_cstr(std::wstring_view wstr);
+
+[[deprecated("leaky")]] gchar* wsview_to_char(std::wstring_view wstr);
+
+std::wstring sview_to_wstr(std::string_view str);
+inline std::wstring gstr_to_wstr(GString* const str)
+{
+    return sview_to_wstr({str->str, str->len});
+}
--- plugins/windows-notification/api/src/converter.cpp
+++ plugins/windows-notification/api/src/converter.cpp
@@ -5,15 +5,63 @@
 // Convert a wide Unicode string to an UTF8 string
 char* wsview_to_char(const std::wstring_view wstr)
 {
-    if(wstr.empty())
-    {
-        return nullptr;
-    }
+    return wsview_to_cstr(wstr).release();
+}
+
+template<typename String>
+static String wsview_to(const std::wstring_view wstr)
+{
     int final_size = WideCharToMultiByte(CP_UTF8, 0, wstr.data(), (int)wstr.size(), nullptr, 0, nullptr, nullptr);
-    gchar* strTo = g_new0(gchar, final_size + 1);
-    WideCharToMultiByte(CP_UTF8, 0, wstr.data(), (int)wstr.size(), strTo, final_size, nullptr, nullptr);
+    String strTo(final_size, '\0');
+    WideCharToMultiByte(CP_UTF8, 0, wstr.data(), (int)wstr.size(), strTo.data(), final_size, nullptr, nullptr);
     return strTo;
 }
+
+std::string wsview_to_str(const std::wstring_view wstr)
+{
+    return wsview_to<std::string>(wstr);
+}
+
+glib::string_ptr wsview_to_gstr(const std::wstring_view wstr)
+{
+    struct adaptor
+    {
+        glib::string_ptr s;
+
+        adaptor(const std::size_t sz, char /*ignored_fill*/)
+            : s{g_string_sized_new(sz)}
+        {
+            g_string_set_size(s.get(), sz);
+        }
+
+        auto data() const { return s->str; }
+        auto size() const { return s->len; }
+    };
+    return wsview_to<adaptor>(wstr).s;
+}
+
+glib::unique_ptr<gchar[]> wsview_to_cstr(const std::wstring_view wstr)
+{
+    struct adaptor
+    {
+        glib::unique_ptr<gchar[]> s;
+        std::size_t sz;  // not counting the terminator
+
+        adaptor(const std::size_t sz, char /*ignored_fill*/)
+            : s{g_try_new(gchar, sz+1)}  // +1 for the terminator
+            , sz{sz}
+        {
+            if (not s)
+                throw std::bad_alloc{};
+            s[sz] = '\0';
+        }
+
+        auto data() const { return s.get(); }
+        auto size() const { return sz; }
+    };
+    return wsview_to<adaptor>(wstr).s;
+}
+
 
 // Convert an UTF8 string to a wide Unicode String
 std::wstring sview_to_wstr(const std::string_view str)
